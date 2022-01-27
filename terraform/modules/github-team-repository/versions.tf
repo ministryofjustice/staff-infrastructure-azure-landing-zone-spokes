4ce6dc8ba4ca66a59b036d34f8d90b2c02efd2e1
Agoney Garcia-Deniz <16398278+reallydontask@users.noreply.github.com>
2022-01-27 15:58:50 +0000

    Just testing sourcing template from other repo

--- terraform/modules/github-team-repository/versions.tf
+++ terraform/modules/github-team-repository/versions.tf
@@ -14,7 +14,7 @@ terraform {
 
 #From looking at the code, It seems that you need organization instead of owner
 provider "github" {
-  token = var.github_token # may as well go with environment variables for both
+ # token = var.github_token # may as well go with environment variables for both
   owner = "ministryofjustice" # This does not work. you need an environment variable
 }
 
